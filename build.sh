a79d508dd95d6d9604b9faf972434e703a53e5e6
mooncr26-rgb <mooncr26@gmail.com>
2026-06-26 12:09:10 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-pip install -r requirements.txt
+# ბიბლიოთეკების ინსტალაცია გლობალურად
+pip install -r requirements.txt --user
+
+# დარწმუნება, რომ Gunicorn-ის ბილიკი სისტემისთვის ხილვადია
+export PATH=$PATH:$HOME/.local/bin
+
+# FFmpeg-ის გადმოწერა
 mkdir -p ffmpeg
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C ffmpeg
