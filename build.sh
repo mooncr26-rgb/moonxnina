3779e0f55300b7de6b0dede9f9861af76b451778
mooncr26-rgb <mooncr26@gmail.com>
2026-06-26 12:03:55 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-# შეცდომის შემთხვევაში პროცესის გაჩერება
-set -o errexit
-
-# ბიბლიოთეკების ინსტალაცია
 pip install -r requirements.txt
-
-# FFmpeg static ბინარების ჩამოტვირთვა და მომზადება
 mkdir -p ffmpeg
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz | tar -xJ --strip-components=1 -C ffmpeg
